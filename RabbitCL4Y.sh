51fb9dd500120a48210d93733111fc11184c6029
RabbitCL4Y <44805762+RabbitCL4Y@users.noreply.github.com>
2018-12-22 12:46:34 +0800

    Create RabbitCL4Y.sh

---
+++ RabbitCL4Y.sh
@@ -0,0 +1,338 @@
+﻿#Kalo mau recode izin dulu sama author
+#Bisa hubungi lewat Email: RabbitCL4Y@gmail.com
+#Author: MR.CL4Y
+#!/system/xbin/bash
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+ungu="\033[35;1m"
+cyan="\033[36;1m"
+white="\033[37;1m"
+red="\033[31;1m"
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                             |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+echo
+echo $red
+pkg install toilet -y
+clear
+echo $green
+pkg install figlet -y
+clear
+echo $cyan
+pkg install ruby -y
+clear
+gem install figlet
+clear
+gem install lolcat
+clear
+pkg install curl
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+sleep 1
+toilet -f future "    Install selesai" | lolcat
+sleep 5
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future " Wellcome to my tools" | lolcat
+sleep 5
+clear
+sh 77
+# Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+###################################################
+# CTRL C-
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $red"[#]> (Ctrl + C ) Detected, 
+Trying To Exit ... "
+sleep 1
+echo ""
+echo  $yellow"[#]> Thank You For Using My 
+Tools ... "
+sleep 1
+echo ""
+echo  $white"[#]> Rabbit Cyber Team Was 
+Here ... "
+read enter
+exit
+}
+=================================================
+echo $yellow
+figlet -f smshadow "TOOLS MR.CL4Y"
+echo $indigo
+date
+echo $purple"___________________________________________________"
+echo $cyan" ["$red"+"$cyan"]Author by	: MR.CL4Y "
+echo $cyan" ["$red"+"$cyan"]Team        : Rabbit Cyber Team		"
+echo $cyan" ["$red"+"$cyan"]Github	: https://github.com/RabbitCL4Y"
+echo $purple"___________________________________________________"
+echo $cyan" 1) install DDoS MR.CL4Y"
+echo $green" 2) install Brute-Webdav-MR.CL4Y"
+echo $yellow" 3) install tools installer v1"
+echo $purple" 4) install tools installer v2"
+echo $red" 5) install tools installer v3"
+echo $white" 6) install tools RCT"
+echo $blue" 7) install tools TUANB4DUT"
+echo $green" 8) install tools Cyb3r_Gh0st"
+echo $purple" 9) install DarkFly-Tool"
+echo $yellow" 10) install tools MR.TR3V!0N"
+echo  $red" 0) Exit "
+echo  $white""
+read -p "[root@MRCL4Y]> " bos;
+
+if [ $bos = 1 ] || [ $bos = 1 ]
+then
+clear
+echo $cyan"install DDoS MR.CL4Y"
+sleep 1
+pkg update && pkg upgrade
+pkg install git 
+pkg install python2
+git clone https://github.com/RabbitCL4Y/CL4y-DDoS
+mv -f CL4y-DDoS $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 2 ] || [ $bos = 3 ]
+then
+clear
+echo $green"install brute-webdav"
+sleep 2
+pkg update && pkg upgrade
+pkg install git
+pkg install sh
+pkg install php
+git clone https://github.com/RabbitCL4Y/brute-webdav
+mv -f brute-webdav $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [$bos = 3 ] || [ $bos = 3 ]
+then
+clear
+echo $yellow"install tools installer v1"
+sleep 3
+pkg update && pkg upgrade
+pkg install git 
+pkg install sh
+git clone https://github.com/RabbitCL4Y/CL4y
+mv -f CL4y $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 4 ] || [ $bos = 4 ]
+then
+clear
+echo $purple"install tools installer v2"
+sleep 1
+pkg update && pkg upgrade
+pkg install git 
+pkg install sh
+git clone
+https://github.com/RabbitCL4Y/CL4y-v2
+mv -f CL4y-v2 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 5 ] || [ $bos = 5 ]
+then
+clear
+echo $red"installing tools installer v3"
+sleep 2
+pkg update && pkg upgrade
+pkg install git 
+pkg install sh
+git clone
+https://github.com/RabbitCL4Y/CL4Yv3
+mv -f CL4Yv3 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 6 ] || [ $bos = 6 ]
+then
+clear
+echo $white"installing tools RCT"
+sleep 3
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/RabCT/rct
+mv -f rct $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 7 ] || [ $bos = 7 ]
+then
+clear
+echo $blue"installing tools TUANB4DUT"
+sleep 1
+apt update && apt upgrade
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+mv -f TOOLSINSTALLERv4 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 8 ] || [ $bos = 8 ]
+then
+clear
+echo "installing tools Cyb3r_Gh0st"
+sleep 2
+pkg update && pkg upgrade
+pkg install figlet
+pkg install php
+pkg install python python2 vim figlet curl
+pip2 install lolcat
+pkg install git
+git clone
+https://github.com/cyberghost69/bima-bagus-jabang-v3
+mv -f bima-bagus-jabang-v3 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 9 ] || [ $bos = 9 ]
+then
+clear
+echo $purple"installing tools DarkFly-Tool"
+sleep 3
+apt upgrade
+apt update
+pkg install python2
+pkg install git
+pkg install php
+git clone https://github.com/Ranginang67/DarkFly-Tool
+mv -f DarkFly-Tool $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 10 ] || [ $bos = 10 ]
+then
+clear
+echo $yellow"installing tools MR.TR3V!0N"
+sleep 4
+apt update && apt upgrade
+pkg install figlet
+pkg install lolcat
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install unzip
+pkg install python
+pkg install python2
+pkg install git
+git clone https://github.com/Rusmana-ID/rus
+mv -f rus $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 0 ] || [ $bos = 0 ]
+then
+echo " Thanks To Rabbit Cyber Team "
+sleep 1
+echo " MR.CL4Y Was Here "
+sleep 1
+echo " Fuck You "
+sleep 1
+echo " MDFK "
+sleep 1
+echo " BITCH "
+sleep 1
+echo " Script Kiddie "
+sleep 1
+exit
+fi
